33a3aa739943647023c6924220ed864799040266
JesseSound <jesseearlemccormack@gmail.com>
2023-10-16 14:40:41 -0400

    Add project files.

---
+++ Assets/Script/Testy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Don't worry it's ok");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
